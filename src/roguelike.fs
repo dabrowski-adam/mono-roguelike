d45d6994aee88d47cc2b027df1907e958410a7f4
Adam Dąbrowski <contact@adamdabrowski.com>
2018-02-23 07:37:47 +0100

    Fix DLL reference

--- src/roguelike.fs
+++ src/roguelike.fs
@@ -1,7 +1,27 @@
 open System
+// open Mono
+open Unix.Terminal
 
+let curse = 
+    // Standard Init sequence
+    Curses.initscr ()
+    Curses.cbreak ()
+    Curses.noecho ()
+    // Recommended
+    Curses.nonl ()
+
+    Curses.addstr ("acción")
+    Curses.refresh ()
+    Curses.getch ()
+    
+    Curses.Window.Standard.intrflush (false)
+    Curses.Window.Standard.keypad (true)
+    // Shutdown
+    Curses.endwin ()
+    
 [<EntryPoint>]
 let main argv = 
     printfn "Hello World" 
+    curse
     Console.ReadLine() |> ignore
     0
