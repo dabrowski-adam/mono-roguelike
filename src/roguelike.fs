dc8a4a2a466ef0762bdaf60bfd564bbd8a12e411
Adam Dąbrowski <contact@adamdabrowski.com>
2018-02-23 11:00:34 +0100

    Add basic game loop

--- src/roguelike.fs
+++ src/roguelike.fs
@@ -1,27 +1,38 @@
-open System
-// open Mono
+#if INTERACTIVE
+#r "../lib/mono-curses.dll"
+#endif
+
 open Unix.Terminal
-
-let curse = 
-    // Standard Init sequence
-    Curses.initscr ()
-    Curses.cbreak ()
-    Curses.noecho ()
-    // Recommended
-    Curses.nonl ()
-
-    Curses.addstr ("acción")
-    Curses.refresh ()
-    Curses.getch ()
     
+let rec loop input =
+    match input with
+    | 27 -> 0
+    | _  -> Curses.move (10, 10)
+            Curses.addch (int '@')
+            loop <| Curses.getch ()
+
+let setup =
+    // Initialisation
+    Curses.initscr ()
+    Curses.cbreak () 
+    Curses.noecho () 
+    Curses.nonl ()   
+    // Config
     Curses.Window.Standard.intrflush (false)
-    Curses.Window.Standard.keypad (true)
+    Curses.Window.Standard.keypad (true) 
+
+[<EntryPoint>]
+let main argv =  
+    setup // initialise Curses
+    
+    Curses.addstr ("""Welcome to the game.
+Press any key to start.
+If you want to quit press 'q' or 'Q'""")
+    Curses.refresh ()                   
+
+    let input = Curses.getch ()
+    loop input
+
     // Shutdown
     Curses.endwin ()
-    
-[<EntryPoint>]
-let main argv = 
-    printfn "Hello World" 
-    curse
-    Console.ReadLine() |> ignore
     0
